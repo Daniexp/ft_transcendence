a5f92e5bbda785a72f877e5fb48c411c92c82265
Daniel Exposito lopez <dexposit@c1r11s5.42madrid.com>
2024-05-26 21:20:42 +0200

    basic container to run a local website using Django and PostgreSQL as a database with bootstrap for the frontend

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 set -e
 
+# Fix settings file
+fix_settings_file() {
+    SETTINGS_FILE="/app/transendence/settings.py"
+    if ! grep -q "import os" "$SETTINGS_FILE"; then
+        sed -i '1i import os' "$SETTINGS_FILE"
+    fi
+}
+
+# Perform initialization if manage.py does not exist
 if [ ! -f "/app/manage.py" ]; then
     django-admin startproject transendence /app
-    # Update settings.py to configure the database
     sed -i "s/'ENGINE': 'django.db.backends.sqlite3'/'ENGINE': 'django.db.backends.postgresql'/g" /app/transendence/settings.py
     sed -i "s/'NAME': BASE_DIR \/ 'db.sqlite3'/'NAME': 'transendence'/g" /app/transendence/settings.py
     sed -i "/'NAME': 'transendence'/a\        'USER': 'user',\n        'PASSWORD': 'password',\n        'HOST': 'db',\n        'PORT': '5432'," /app/transendence/settings.py
     echo "ALLOWED_HOSTS = ['*']" >> /app/transendence/settings.py
     echo "STATIC_ROOT = os.path.join(BASE_DIR, 'static')" >> /app/transendence/settings.py
+    fix_settings_file
     python manage.py migrate
     python manage.py collectstatic --noinput
+else
+    fix_settings_file
 fi
 
 exec "$@"
